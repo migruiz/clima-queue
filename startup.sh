10750abb63b9145c71df3d153a6aa3abd386b773
Miguel Ruiz <miguel@dpsystems.ie>
2018-07-17 18:07:08 +0100

    starting service

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rabbitmq-plugins enable rabbitmq_management;
-service rabbitmq-server start;
+rabbitmq-server start;
 rabbitmqctl add_user pi pi;
 rabbitmqctl set_user_tags pi;
 rabbitmqctl set_permissions -p / pi ".*" ".*" ".*"
