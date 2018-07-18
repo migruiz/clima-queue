10137eaf90f04cd47f61a81a8d286a0161efd9ec
Miguel Ruiz <miguel@dpsystems.ie>
2018-07-18 12:58:30 +0100

    creating sysadnub

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 rabbitmq-plugins enable rabbitmq_management;
 service rabbitmq-server start;
 rabbitmqctl add_user pi pi;
-rabbitmqctl set_user_tags pi;
+rabbitmqctl set_user_tags pi administrator;
 rabbitmqctl set_permissions -p / pi ".*" ".*" ".*";
 service rabbitmq-server stop
 rabbitmq-server start
